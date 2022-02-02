34bc513361849757a464d2218e5327f57b18440c
DwEnn <prgohn@gmail.com>
2022-02-02 17:38:22 +0900

    feat: unit test

---
+++ app/src/test/java/com/example/android/codelabs/naviagtion/ExampleTest.kt
@@ -0,0 +1,12 @@
+package com.example.android.codelabs.naviagtion
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class ExampleTest {
+
+    @Test
+    fun test() {
+        Assertions.assertEquals(4+4, 4)
+    }
+}
